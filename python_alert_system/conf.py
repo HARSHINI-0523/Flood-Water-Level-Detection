9f84e244a39877ea575daf949e06a8abb364fa7d
GOPISETTY HARSHINI <149379234+HARSHINI-0523@users.noreply.github.com>
2025-04-24 00:20:56 +0530

    conf.py

---
+++ python_alert_system/conf.py
@@ -0,0 +1,11 @@
+# conf.py
+
+# Twilio credentials
+SID = 'Your_Twilio_SID'
+AUTH_TOKEN = 'Your_Twilio_AUTH_TOKEN'
+FROM_NUMBER = 'Your_Twilio_Phone_Number'
+TO_NUMBER = 'Receiver_Phone_Number'
+
+# Bolt IoT credentials
+API_KEY = 'Your_Bolt_API_Key'
+DEVICE_ID = 'Your_Bolt_Device_ID'
